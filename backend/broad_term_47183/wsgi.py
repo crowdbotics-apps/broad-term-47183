8ff6349e5a7049d52dc1f6bed0eae35ea8dcd138
Crowdbotics <team@crowdbotics.com>
2024-03-21 14:12:27 +0000

    Initial commit

---
+++ backend/broad_term_47183/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for broad_term_47183 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "broad_term_47183.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-term-47183",
+  url: "https://broad-term-47183.botics.co",
+  api: "https://broad-term-47183.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
